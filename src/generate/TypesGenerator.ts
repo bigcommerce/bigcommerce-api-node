62e02811b7d9c07767730386c3965c7832e94de3
Maria Navarro <maria.navarro@bigcommerce.com>
2023-07-19 13:30:19 -0500

    fix: APPEX-585 update catalog typings

--- src/generate/TypesGenerator.ts
+++ src/generate/TypesGenerator.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 import { assertIsError } from '../utils/assertIsError';
 
-import { GitHubRepoTree } from './types';
+import { GitHubRepoTree, NestedTree } from './types';
 
 const { writeFile } = promises;
 
@@ -14,6 +14,7 @@ class TypesGenerator {
 
   private REPO_URL = 'https://api.github.com/repos/bigcommerce/api-specs/git/trees/main';
   private CONTENT_URL = 'https://raw.githubusercontent.com/bigcommerce/api-specs/main/reference/';
+  // private CONTENT_URL = 'https://raw.githubusercontent.com/bigcommerce/api-specs/main';
 
   private OUTPUT_DIRECTORY_PATH = path.join(process.cwd(), 'src/generate/generated');
   private PRETTIER_CONFIG_PATH = path.join(process.cwd(), '.prettierrc');
@@ -32,6 +33,8 @@ class TypesGenerator {
    */
   public async generate(): Promise<void> {
     const sourceFiles = await this.getSourceFiles();
+
+    // console.log(sourceFiles, 'here the source files');
 
     for (const sourceFile of sourceFiles) {
       const outputPath = path.join(
@@ -63,14 +66,49 @@ class TypesGenerator {
    */
   private async getSourceFiles(): Promise<string[]> {
     const sourceRepo = await this.client.get<GitHubRepoTree>(this.REPO_URL);
+    // console.log(sourceRepo, 'here the source repo');
+
+    // console.log(sourceRepo.data.tree, 'here the tree');
     const targetDir = sourceRepo.data.tree.find(file => file.path === 'reference');
+    // // console.log(targetDir, 'here teh targetDir');
+    // const targetDir = sourceRepo.data.tree.
+    // console.log(targetDir, 'here the target DIR');
 
     if (!targetDir) {
       throw new Error('Unable to find a directory containing Open API spec files in provided repo');
     }
 
+    const specFileNames = await this.getSpecFileNames(targetDir);
+
+    // const remoteSourceDir = await this.client.get<GitHubRepoTree>(targetDir.url);
+    // const specFileNames = remoteSourceDir.data.tree.map(file => `${this.CONTENT_URL}${file.path}`);
+
+    return specFileNames;
+  }
+
+  private async getSpecFileNames(targetDir?: NestedTree): Promise<string[]> {
+    if (!targetDir) {
+      return [];
+    }
+
     const remoteSourceDir = await this.client.get<GitHubRepoTree>(targetDir.url);
-    const specFileNames = remoteSourceDir.data.tree.map(file => `${this.CONTENT_URL}${file.path}`);
+
+    const specFileNames = await remoteSourceDir.data.tree.reduce(
+      async (accPromise: Promise<string[]>, node: NestedTree): Promise<string[]> => {
+        const acc = await accPromise;
+
+        if (node.type === 'blob') {
+          return targetDir.path === 'reference'
+            ? [...acc, `${this.CONTENT_URL}${node.path}`]
+            : [...acc, `${this.CONTENT_URL}${targetDir.path}/${node.path}`];
+        } else {
+          const nestedSpecFileNames: string[] = await this.getSpecFileNames(node);
+
+          return [...acc, ...nestedSpecFileNames];
+        }
+      },
+      Promise.resolve([]),
+    );
 
     return specFileNames;
   }
